93818e4360da05582602a485c18d19afb4b1f06c
Jeroen Valcke <valcke_jeroen@hotmail.com>
2017-05-22 22:08:22 +0200

    Allow adding additional proxy routes.

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 "use strict";
 
 require("rootpath")();
+var cloneDeep = require("lodash.cloneDeep");
+var get = require("lodash.get");
 var merge = require("lodash.merge");
 var proxy = require("http-proxy").createProxyServer({});
 
 var generateConfig = function generateConfig(config) {
-	var suffix = config.suffix || "proxy";
+	var suffix = config.suffix || "/proxy";
 
-	merge({},
-		{
-			options: {
-				changeOrigin: true
+	return {
+		target: config.target,
+		changeOrigin: true,
+		headers: merge({},
+			{
+				host: config.host,
+				apikey: config.apikey,
+				tenant: config.tenant
 			},
-			routes: [
-				{
-					target: "files/",
-					route: "suffix"
-				},
-				{
-					target: "files/",
-					route: [
-						"/files",
-						"/file",
-						"/" + suffix + "/files",
-						"/" + suffix +"/file",
-						"/api/1.0.0/files",
-						"/api/1.0.0/file"
-					]
-				}
-			]
-		},
-		config
-	);
+			config.headers
+		),
+		routes: [
+			{
+				target: "",
+				route: suffix
+			},
+			{
+				target: "files/",
+				route: [
+					"/files",
+					"/file",
+					"/" + suffix + "/files",
+					"/" + suffix +"/file",
+					"/api/1.0.0/files",
+					"/api/1.0.0/file"
+				]
+			}
+		]
+	};
 };
 
 var checkConfig = function checkConfig(config) {
@@ -46,31 +52,30 @@ var fallback = function fallback(target) {
     };
 };
 
-var generateConfig = function generateConfig(app, target, apikey, tenant, host) {
+var convertParams = function convertParams(app, target, apikey, tenant, host, suffix) {
      return {
         target: target,
-        changeOrigin: true,
-        headers: {
-            host: host,
-            apikey: apikey,
-            tenant: tenant
-        }
+		host: host,
+		apikey: apikey,
+		tenant: tenant
     };
 };
 
 var proxyMiddleware = function(config, route) {
-	return function(req, res, next) {
-		delete config.routes;
+	config.target += get(route, "target", "");
+	delete config.routes;
 
+	return function(req, res, next) {
 		return proxy.web(req, res, config, fallback);
 	};
 };
 
-module.exports = function (app, params) {
+var main = function main(app, params) {
 	var c;
+	var args = [].slice.call(arguments);
 
-    if (arguments.length >= 4) {
-        c = generateConfig.apply(arguments);
+    if (args.length >= 4) {
+        c = convertParams.apply(args[0], args);
     } else {
 		c = params;
 	}
@@ -78,6 +83,12 @@ module.exports = function (app, params) {
 	var config = generateConfig(c);
 
 	for (var i = 0; i < config.routes.length; i++) {
-		app.use(config.routes[i].routes, proxyMiddleware(config, config.routes[i]));
+		app.use(config.routes[i].route, proxyMiddleware(cloneDeep(config), config.routes[i]));
 	}
 };
+
+main.addProxyRoute = function(app, routes, proxyConfig) {
+	app.use(routes, proxyMiddleware(cloneDeep(generateConfig(proxyConfig))));
+};
+
+module.exports = main;
